db116ac3ee155c51016e016ac9370c2d2f4e23ab
SSSCFW <85436308+SSSCFW@users.noreply.github.com>
2026-09-12 14:31:53 +0900

    fix: keep infinite axe unbreakable in combat

--- src/main/java/dev/ssscfw/advancedtools/item/UpgradedAxeItem.java
+++ src/main/java/dev/ssscfw/advancedtools/item/UpgradedAxeItem.java
@@ -46,6 +46,13 @@ public class UpgradedAxeItem extends AxeItem implements AreaMiningItem {
     }
 
     @Override
+    public void postHurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        if (!infinite) {
+            super.postHurtEnemy(stack, target, attacker);
+        }
+    }
+
+    @Override
     public InteractionResult useOn(UseOnContext context) {
         if (!infinite) {
             return super.useOn(context);
